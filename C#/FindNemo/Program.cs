19a7f748c4c1ce1b6d44834c218a9c16493b3cf8
Shayar Shrestha <shayarshrestha23@gmail.com>
2021-11-16 23:16:07 +0545

    Calculating performance in c# and js

--- C#/FindNemo/Program.cs
+++ C#/FindNemo/Program.cs
@@ -12,10 +12,15 @@ namespace FindNemo
 
     static void FindNemo(string[] array)
     {
+      var watch = new System.Diagnostics.Stopwatch();
+
+      watch.Start();
       for (int i = 0; i < (int)array.Length; i++)
       {
         if (array[i] == "nemo") Console.WriteLine("FoundNemo!");
       }
+      watch.Stop();
+      Console.WriteLine($"{ watch.ElapsedMilliseconds} ms");
     }
   }
 }
--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,14 @@
+const { PerformanceObserver, performance } = require("perf_hooks");
+
 const nemo = ["nemo"];
 
 function findNemo(array) {
+  let t0 = performance.now();
   for (let i = 0; i < array.length; i++) {
     if (array[i] === "nemo") console.log("FoundNemo!");
   }
+  let t1 = performance.now();
+  console.log("Call to find nemo took " + (t1 - t0) + "milliseconds");
 }
 
 findNemo(nemo);
