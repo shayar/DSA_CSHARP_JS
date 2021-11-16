553f08fab2c88d449d1df39d1d7a17b3be7530b9
Shayar Shrestha <shayarshrestha23@gmail.com>
2021-11-16 23:05:06 +0545

    Find Nemo

---
+++ C#/FindNemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FindNemo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] nemo = new string[] { "nemo" };
+      Program.FindNemo(nemo);
+    }
+
+    static void FindNemo(string[] array)
+    {
+      for (int i = 0; i < (int)array.Length; i++)
+      {
+        if (array[i] == "nemo") Console.WriteLine("FoundNemo!");
+      }
+    }
+  }
+}
